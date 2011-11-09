ed08aea4f1c7b66c6d6b0aa09b9cf9a50bc6e576
Phips Peter <pspeter333@gmail.com>
2011-11-09 12:13:28 -0600

    Updated the Ip spec

--- spec/models/ip_spec.rb
+++ spec/models/ip_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
-describe "Ip Model" do
+describe Ip do
   let(:ip) { Ip.new }
   it 'can be created' do
     ip.should_not be_nil
   end
+  it { should have_field(:addr).of_type(String) }
+  it { should validate_uniqueness_of(:addr) }
+  it { should have_index_for(:addr).with_options(:unqiue => true) }
 end
