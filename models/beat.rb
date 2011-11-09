81a36f5fd83d276fd863a35302437fa0897ac473
Phips Peter <pspeter333@gmail.com>
2011-11-09 12:49:47 -0600

    Type is now added to tags by default

--- models/beat.rb
+++ models/beat.rb
@@ -1,8 +1,9 @@
 class Beat
   include Mongoid::Document
-  include Mongoid::Tagable
+  include Mongoid::Taggable
   include Mongoid::FullTextSearch
   include Mongo::Voteable
+  include Mongoid::Timestamps
   
   field :type, :type => String
   field :timestamp, :type => Time
@@ -18,4 +19,11 @@ class Beat
   
   fulltext_search_in :summary
   voteable self, :up => +1, :down => -1
+  
+  before_create :add_type_to_tags
+  
+  private
+  def add_type_to_tags
+    self.tags = self.type
+  end
 end
