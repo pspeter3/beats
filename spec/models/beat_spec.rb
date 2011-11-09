2daa53d885d61855386b924009c23b53ddc34a68
Phips Peter <pspeter333@gmail.com>
2011-11-09 12:27:51 -0600

    updated the spec

--- spec/models/beat_spec.rb
+++ spec/models/beat_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
-describe "Beat Model" do
+describe Beat do
   let(:beat) { Beat.new }
   it 'can be created' do
     beat.should_not be_nil
   end
+  it { should have_fields(:type, :location, :summary).of_type(String) }
+  it { should have_field(:timestamp).of_type(Time) }
+  it { should validate_presence_of(:type) }
+  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:summary) }
+  it { should validate_presence_of(:timestamp) }
 end
