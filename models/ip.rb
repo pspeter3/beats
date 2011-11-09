777a7d027b91f04c08b620eecf5fc615ebf92179
Phips Peter <pspeter333@gmail.com>
2011-11-09 12:19:26 -0600

    Added presence validation for ip

--- models/ip.rb
+++ models/ip.rb
@@ -4,6 +4,7 @@ class Ip
 
   field :addr, :type => String
   
+  validates_presence_of :addr
   validates_uniqueness_of :addr
   
   index :addr, :unique => true
--- spec/models/ip_spec.rb
+++ spec/models/ip_spec.rb
@@ -6,6 +6,6 @@ describe Ip do
     ip.should_not be_nil
   end
   it { should have_field(:addr).of_type(String) }
+  it { should validate_presence_of(:addr) }
   it { should validate_uniqueness_of(:addr) }
-  it { should have_index_for(:addr).with_options(:unqiue => true) }
 end
