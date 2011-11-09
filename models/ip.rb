0e42e2a25a45df941a61ad97a1e2d7b33ec71905
Phips Peter <pspeter333@gmail.com>
2011-11-09 12:15:04 -0600

    Specs now pass for IP

--- models/ip.rb
+++ models/ip.rb
@@ -1,13 +1,10 @@
 class Ip
   include Mongoid::Document
-  include Mongoid::Timestamps # adds created_at and updated_at fields
+  include Mongo::Voter
 
-  # field <name>, :type => <type>, :default => <value>
   field :addr, :type => String
-
-  # You can define indexes on documents using the index macro:
-  # index :field <, :unique => true>
-
-  # You can create a composite key in mongoid to replace the default id using the key macro:
-  # key :field <, :another_field, :one_more ....>
+  
+  validates_uniqueness_of :addr
+  
+  index :addr, :unique => true
 end
