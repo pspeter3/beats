1cc2bf006b2cd1366b8ad4bb589656aad80afab6
Phips Peter <pspeter333@gmail.com>
2011-11-09 12:20:39 -0600

    Updated the comment spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Comment do
+  let(:comment) { Comment.new }
+  it 'can be created' do
+    comment.should_not be_nil
+  end
+  it { should have_field(:body).of_type(String) }
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).within(1..140) }
+  it { should be_timestamped_document }
+end
