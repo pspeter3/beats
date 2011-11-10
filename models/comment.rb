3a7c052d55675236d09be754a416ccd404dc09bf
Phips Peter <pspeter333@gmail.com>
2011-11-09 18:04:00 -0600

    Added a name to the comment model

--- models/comment.rb
+++ models/comment.rb
@@ -2,9 +2,12 @@ class Comment
   include Mongoid::Document
   include Mongoid::Timestamps::Created
   
+  field :name, :type => String
   field :body, :type => String
   field :approved, :type => Boolean, :default => false
   
+  validates_presence_of :name
+  validates_length_of :name, :minimum => 1, :maximum => 16
   validates_presence_of :body
   validates_length_of :body, :minimum => 1, :maximum => 140
   
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,10 @@ describe Comment do
   it 'can be created' do
     comment.should_not be_nil
   end
+  it { should have_field(:name).of_type(String) }
   it { should have_field(:body).of_type(String) }
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).within(1..16) }
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:body).within(1..140) }
   it { should have_field(:approved).of_type(Boolean).with_default_value_of(false) }
