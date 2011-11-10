c7f68c1b1adfd1b2d9dbad1c328bc1377642ff26
Phips Peter <pspeter333@gmail.com>
2011-11-10 13:36:15 -0600

    Began the parsing of the website

--- app/workers/scraper.rb
+++ app/workers/scraper.rb
@@ -1,4 +1,14 @@
 class Scraper
-  def self.run(latest)
+  # Parse 
+  def parse(latest)
+    # Open the HTML document
+    doc = Nokogiri::HTML open('http://police.wustl.edu/mediatest.html')
+    # Get the relevant paragraphs
+    beats = doc.css('tr p')
+    # Filter down to the beats
+    beats.reject! { |i| !(i =~ /location/i) }
+    # Map to the arrays
+    beats.map! { |i| i.inner_text.split(/\r\n/) }
+    
   end
 end
