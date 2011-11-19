59f305ed4e5b3f97d228c176214a3997383a7880
Phips Peter <pspeter333@gmail.com>
2011-11-18 23:43:04 -0600

    Added show method

--- app/controllers/beats.rb
+++ app/controllers/beats.rb
@@ -1,27 +1,10 @@
 Beats.controllers :beats do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
-
   get :index do
   end
 
-  get :show do
+  get :show, :map => '/beats/:id', :provides => :json do
+    @beat = Beat.find params[:id]
+    
+    render 'beats/show'
   end
-
 end
