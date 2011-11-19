73fe6a0f147a92d3b6c69fc90ef47c1f478933d3
Phips Peter <pspeter333@gmail.com>
2011-11-18 23:37:08 -0600

    Temporarily pausing work on the scraper

--- app/workers/scraper.rb
+++ app/workers/scraper.rb
@@ -1,6 +1,6 @@
 class Scraper
   # Parse 
-  def parse(latest)
+  def parse
     # Open the HTML document
     doc = Nokogiri::HTML open('http://police.wustl.edu/mediatest.html')
     # Get the relevant paragraphs
@@ -9,6 +9,21 @@ class Scraper
     beats.reject! { |i| !(i =~ /location/i) }
     # Map to the arrays
     beats.map! { |i| i.inner_text.split(/\r\n/) }
+    # Iterate through the beats
+    beats.each do |record|
+      # Get an instance of the beat model 
+      beat = handle record
+      
+    end
+  end
+  
+  def handle(record)
+    beat = Beat.new
+    beat.crime, beat.timestamp = get_crime_and_timestamp record[0]
     
+  end
+  
+  def get_crime_and_timestamp(line)
+     
   end
 end
