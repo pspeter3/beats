7ade1c4319e5ea5e16da0563dea88e61d2fe76e4
Phips Peter <pspeter333@gmail.com>
2011-12-05 12:02:53 -0600

    Removed the comments controller

--- app/controllers/comment.rb
+++
@@ -1,9 +0,0 @@
-Beats.controllers :comment, :parent => :beats do
-  get :new do
-    beat = Beat.find(params[:beat_id])
-    comment = Comment.new(:name => params[:name], :body => params[:body])
-    beat.comments << comment if comment.errors.size == 0
-    
-    jsonp comment.errors
-  end
-end
